0a686f0d961268187ba709f2c30d9a057ad6f1ac
yamorge <yamorge@gmail.com>
2024-12-23 14:02:48 +0300

    Initial commit

---
+++ src/main/java/org/example/LoginApp.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.example.gui.LoginGUI;
+
+public class LoginApp {
+    public static void main(String[] args) {
+        new LoginGUI().setVisible(true);
+    }
+}
---
+++ src/main/java/org/example/constants/CommonConstants.java
@@ -0,0 +1,7 @@
+package org.example.constants;
+
+public class CommonConstants {
+    public static final String APP_NAME = "Login App";
+    public static final int[] FRAME_SIZE = {400, 400};
+    public static final int TEXTFIELD_SIZE = 15;
+}
